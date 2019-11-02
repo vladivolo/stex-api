518cbb959d65216344b2bdff0c1ddf53100b4e0c
Вологжанин Владимир Витальевич <vologzhaninvv@altarix.ru>
2019-11-02 16:40:58 +0300

    add deposit and withdrawal

---
+++ deposit.go
@@ -0,0 +1,79 @@
+package stex
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type Deposit struct {
+	id          int64  `json:"id"`
+	Name        string `json:"name"`
+	StatusColor string `json:"color"`
+}
+
+type DepositStatusesService struct {
+	c *Client
+}
+
+// Do send request
+func (s *DepositStatusesService) Do(ctx context.Context, opts ...RequestOption) ([]Deposit, error) {
+	r := &request{
+		method:   "GET",
+		endpoint: fmt.Sprintf("/public/deposit-statuses"),
+		secType:  secTypeNone,
+	}
+
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := struct {
+		APIError
+		Data []Deposit `json:"data"`
+	}{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Data, err
+}
+
+type DepositStatusByIdService struct {
+	c *Client
+
+	id *int64
+}
+
+// Do send request
+func (s *DepositStatusByIdService) Do(ctx context.Context, opts ...RequestOption) (*Deposit, error) {
+	if s.id == nil {
+		return nil, fmt.Errorf("id not init")
+	}
+
+	r := &request{
+		method:   "GET",
+		endpoint: fmt.Sprintf("/public/deposit-statuses/%d", *s.id),
+		secType:  secTypeNone,
+	}
+
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := struct {
+		APIError
+		Data Deposit `json:"data"`
+	}{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res.Data, err
+}
---
+++ withdrawal.go
@@ -0,0 +1,79 @@
+package stex
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type Withdrawal struct {
+	id          int64  `json:"id"`
+	Name        string `json:"name"`
+	StatusColor string `json:"color"`
+}
+
+type WithdrawalStatusesService struct {
+	c *Client
+}
+
+// Do send request
+func (s *WithdrawalStatusesService) Do(ctx context.Context, opts ...RequestOption) ([]Withdrawal, error) {
+	r := &request{
+		method:   "GET",
+		endpoint: fmt.Sprintf("/public/withdrawal-statuses"),
+		secType:  secTypeNone,
+	}
+
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := struct {
+		APIError
+		Data []Withdrawal `json:"data"`
+	}{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Data, err
+}
+
+type WithdrawalStatusByIdService struct {
+	c *Client
+
+	id *int64
+}
+
+// Do send request
+func (s *WithdrawalStatusByIdService) Do(ctx context.Context, opts ...RequestOption) (*Withdrawal, error) {
+	if s.id == nil {
+		return nil, fmt.Errorf("id not init")
+	}
+
+	r := &request{
+		method:   "GET",
+		endpoint: fmt.Sprintf("/public/withdrawal-statuses/%d", *s.id),
+		secType:  secTypeNone,
+	}
+
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := struct {
+		APIError
+		Data Withdrawal `json:"data"`
+	}{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res.Data, err
+}
