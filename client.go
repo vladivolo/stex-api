1129b69268968c369c36b7cecfcd7b8f38424b16
Вологжанин Владимир Витальевич <vologzhaninvv@altarix.ru>
2019-11-03 02:07:38 +0300

    Cosmetics

--- client.go
+++ client.go
@@ -103,6 +103,7 @@ func (c *Client) parseRequest(r *request, opts ...RequestOption) (err error) {
 	}
 
 	if bodyString != "" {
+		header.Set("Content-Type", "application/x-www-form-urlencoded")
 		body = bytes.NewBufferString(bodyString)
 	}
 
@@ -164,114 +165,144 @@ func (c *Client) callAPI(ctx context.Context, r *request, opts ...RequestOption)
 	return data, nil
 }
 
-func (c *Client) NewPingService() *PingService {
-	return &PingService{c: c}
-}
-
+// Get list of avialable currencies.
 func (c *Client) NewAvailableCurrenciesService() *AvailableCurrenciesService {
 	return &AvailableCurrenciesService{c: c}
 }
 
+// Get currency info
 func (c *Client) NewCurrencyInfoByIdService() *CurrencyInfoByIdService {
 	return &CurrencyInfoByIdService{c: c}
 }
 
+// Get list of all avialable markets
 func (c *Client) NewAvailableMarketsService() *AvailableMarketsService {
 	return &AvailableMarketsService{c: c}
 }
 
+// Get list of all avialable currency pairs groups
 func (c *Client) NewPairsGroupsService() *PairsGroupsService {
 	return &PairsGroupsService{c: c}
 }
 
+// Returns a list of avialable currency pairs in the given market if {code} is one of the values returned by /public/markets.
+// Returns all available currency pairs if ALL passed as a {code}
 func (c *Client) NewCurrencyPairsListService() *CurrencyPairsListService {
 	return &CurrencyPairsListService{c: c}
 }
 
+// Returns a list of avialable currency pairs in the given currency pair group
 func (c *Client) NewCurrencyPairsGroupsService() *CurrencyPairsGroupsService {
 	return &CurrencyPairsGroupsService{c: c}
 }
 
+// Get currency pair information
 func (c *Client) NewCurrencyPairInfoService() *CurrencyPairInfoService {
 	return &CurrencyPairInfoService{c: c}
 }
 
+// Returns last 24H information about every currency pair.
 func (c *Client) NewCurrencyPairsTickerService() *CurrencyPairsTickerService {
 	return &CurrencyPairsTickerService{c: c}
 }
 
+// Returns last 24H information about currency pair ticker
 func (c *Client) NewCurrencyPairTickerService() *CurrencyPairTickerService {
 	return &CurrencyPairTickerService{c: c}
 }
 
+// Trades for given currency pair
 func (c *Client) NewCurrencyPairTradesService() *CurrencyPairTradesService {
 	return &CurrencyPairTradesService{c: c}
 }
 
+// Orderbook for given currency pair
 func (c *Client) NewCurrencyPairOrderbookService() *CurrencyPairOrderbookService {
 	return &CurrencyPairOrderbookService{c: c}
 }
 
+// Provides a list of candles for the chart. Candles are always ordered in descending order (the latest are first)
 func (c *Client) NewCurrencyPairChartService() *CurrencyPairChartService {
 	return &CurrencyPairChartService{c: c}
 }
 
+// Get list of avialable deposit statuses.
 func (c *Client) NewDepositStatusesService() *DepositStatusesService {
 	return &DepositStatusesService{c: c}
 }
 
+// Get deposit status info
 func (c *Client) NewDepositStatusByIdService() *DepositStatusByIdService {
 	return &DepositStatusByIdService{c: c}
 }
 
+// Get list of avialable withdrawal statuses.
 func (c *Client) NewWithdrawalStatusesService() *WithdrawalStatusesService {
 	return &WithdrawalStatusesService{c: c}
 }
 
+// Get withdrawal status info
 func (c *Client) NewWithdrawalStatusByIdService() *WithdrawalStatusByIdService {
 	return &WithdrawalStatusByIdService{c: c}
 }
 
+// Test API is working
+func (c *Client) NewPingService() *PingService {
+	return &PingService{c: c}
+}
+
+// Returns the user's fees for a given currency pair
 func (c *Client) NewCurrencyPairFeeService() *CurrencyPairFeeService {
 	return &CurrencyPairFeeService{c: c}
 }
 
+// List of your currently open orders.
 func (c *Client) NewOpenOrdersListService() *OpenOrdersListService {
 	return &OpenOrdersListService{c: c}
 }
 
+// Puts an request to delete all active (processing or pending) orders to orders processing queue
 func (c *Client) NewOpenOrdersDeleteService() *OpenOrdersDeleteService {
 	return &OpenOrdersDeleteService{c: c}
 }
 
+// List of your currently open orders for certain currency pair.
 func (c *Client) NewCurrencyPairOpenOrdersListService() *CurrencyPairOpenOrdersListService {
 	return &CurrencyPairOpenOrdersListService{c: c}
 }
 
+// Puts an request to delete all active (processing or pending) of the given currency pair orders to orders processing queue
 func (c *Client) NewCurrencyPairOpenOrdersDeleteService() *CurrencyPairOpenOrdersDeleteService {
 	return &CurrencyPairOpenOrdersDeleteService{c: c}
 }
 
+// Create new order and put it to the orders processing queue
 func (c *Client) NewCreateOrderService() *CreateOrderService {
 	return &CreateOrderService{c: c}
 }
 
+// Get information about the given order
 func (c *Client) NewOrderInfoService() *OrderInfoService {
 	return &OrderInfoService{c: c}
 }
 
+// Cancel order
 func (c *Client) NewOrderDeleteService() *OrderDeleteService {
 	return &OrderDeleteService{c: c}
 }
 
+// Get the list of closed (finished, partial or cancelled) orders.
+// If WITH_TRADES orderStatus is passed then both PARTIAL and FINISHED orders will be returned in a single run
 func (c *Client) NewOrdersHistoryService() *OrdersHistoryService {
 	return &OrdersHistoryService{c: c}
 }
 
+// Get trades and fees information for given order
 func (c *Client) NewTradesOrderHistoryService() *TradesOrderHistoryService {
 	return &TradesOrderHistoryService{c: c}
 }
 
+// Returns a list of all trades that conform the filters given in a request string
 func (c *Client) NewCurrencyPairTradesHistoryService() *CurrencyPairTradesHistoryService {
 	return &CurrencyPairTradesHistoryService{c: c}
 }
---
+++ history.go
@@ -0,0 +1,250 @@
+package stex
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Trade struct {
+	Id          int64     `json:"id"`
+	BuyOrderId  int64     `json:"buy_order_id"`
+	SellOrderId int64     `json:"sell_order_id"`
+	Price       float64   `json:"price"`
+	Amount      float64   `json:"amount"`
+	TradeType   TradeType `json:"trade_type"`
+	Timestamp   string    `json:"timestamp"`
+}
+
+type Fee struct {
+	Id         int64   `json:"id"`
+	CurrencyId int     `json:"currency_id"`
+	Amount     float64 `json:"amount"`
+	Timestamp  string  `json:"timestamp"`
+}
+
+type TradeOrderDetail struct {
+	Id             int64   `json:""`
+	CurrencyPairId int     `json:"currency_pair_id"`
+	Price          string  `json:"price"`
+	InitialAmount  string  `json:"initial_amount"`
+	Type           string  `json:"type"`
+	Created        string  `json:"created"`
+	Timestamp      int64   `json:"timestamp"`
+	Status         string  `json:"status"`
+	Trades         []Trade `json:"trades"`
+	Fees           []Fee   `json:"fees"`
+}
+
+type OrdersHistoryService struct {
+	c *Client
+
+	pair_id      *int
+	order_status *OrderStatus
+	time_start   *time.Time
+	time_end     *time.Time
+	limit        *int
+	offset       *int
+}
+
+// Do send request
+func (s *OrdersHistoryService) Do(ctx context.Context, opts ...RequestOption) ([]OrderInfo, error) {
+	r := &request{
+		method:   "GET",
+		endpoint: "/reports/orders",
+		secType:  secTypeAPIKey,
+	}
+
+	if s.pair_id != nil {
+		r.setParam("currencyPairId", *s.pair_id)
+	}
+
+	if s.time_start != nil {
+		r.setParam("timeStart", s.time_start.UTC().Format("2006-01-02 15:04:05"))
+	}
+
+	if s.time_end != nil {
+		r.setParam("timeEnd", s.time_end.UTC().Format("2006-01-02 15:04:05"))
+	}
+
+	if s.order_status != nil {
+		r.setParam("order_status", *s.order_status)
+	}
+
+	if s.limit != nil {
+		r.setParam("limit", *s.limit)
+	}
+
+	if s.offset != nil {
+		r.setParam("offset", *s.offset)
+	}
+
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := struct {
+		APIError
+		Data []OrderInfo `json:"data"`
+	}{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Data, err
+}
+
+func (s *OrdersHistoryService) CurrencyPairId(pair_id int) *OrdersHistoryService {
+	s.pair_id = &pair_id
+	return s
+}
+
+func (s *OrdersHistoryService) Status(status OrderStatus) *OrdersHistoryService {
+	s.order_status = &status
+	return s
+}
+
+func (s *OrdersHistoryService) TmStart(from time.Time) *OrdersHistoryService {
+	s.time_start = &from
+	return s
+}
+
+func (s *OrdersHistoryService) TmEnd(end time.Time) *OrdersHistoryService {
+	s.time_end = &end
+	return s
+}
+
+func (s *OrdersHistoryService) Limit(limit int) *OrdersHistoryService {
+	s.limit = &limit
+	return s
+}
+
+func (s *OrdersHistoryService) Offset(offset int) *OrdersHistoryService {
+	s.offset = &offset
+	return s
+}
+
+type TradesOrderHistoryService struct {
+	c *Client
+
+	order_id *int64
+}
+
+// Do send request
+func (s *TradesOrderHistoryService) Do(ctx context.Context, opts ...RequestOption) (*TradeOrderDetail, error) {
+	if s.order_id == nil {
+		return nil, fmt.Errorf("order_id not init")
+	}
+
+	r := &request{
+		method:   "GET",
+		endpoint: fmt.Sprintf("/reports/orders/%d", *s.order_id),
+		secType:  secTypeAPIKey,
+	}
+
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := struct {
+		APIError
+		Data TradeOrderDetail `json:"data"`
+	}{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res.Data, err
+}
+
+func (s *TradesOrderHistoryService) OrderId(id int64) *TradesOrderHistoryService {
+	s.order_id = &id
+	return s
+}
+
+type CurrencyPairTradesHistoryService struct {
+	c *Client
+
+	pair_id    *int
+	time_start *time.Time
+	time_end   *time.Time
+	limit      *int
+	offset     *int
+}
+
+func (s *CurrencyPairTradesHistoryService) Do(ctx context.Context, opts ...RequestOption) ([]Trade, error) {
+	if s.pair_id == nil {
+		return nil, fmt.Errorf("pair_id not init")
+	}
+
+	r := &request{
+		method:   "GET",
+		endpoint: fmt.Sprintf("/reports/trades/%d", s.pair_id),
+		secType:  secTypeAPIKey,
+	}
+
+	if s.time_start != nil {
+		r.setParam("timeStart", s.time_start.UTC().Format("2006-01-02 15:04:05"))
+	}
+
+	if s.time_end != nil {
+		r.setParam("timeEnd", s.time_end.UTC().Format("2006-01-02 15:04:05"))
+	}
+
+	if s.limit != nil {
+		r.setParam("limit", *s.limit)
+	}
+
+	if s.offset != nil {
+		r.setParam("offset", *s.offset)
+	}
+
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	res := struct {
+		APIError
+		Data []Trade `json:"data"`
+	}{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Data, err
+}
+
+func (s *CurrencyPairTradesHistoryService) CurrencyPairId(pair_id int) *CurrencyPairTradesHistoryService {
+	s.pair_id = &pair_id
+	return s
+}
+
+func (s *CurrencyPairTradesHistoryService) TmStart(from time.Time) *CurrencyPairTradesHistoryService {
+	s.time_start = &from
+	return s
+}
+
+func (s *CurrencyPairTradesHistoryService) TmEnd(end time.Time) *CurrencyPairTradesHistoryService {
+	s.time_end = &end
+	return s
+}
+
+func (s *CurrencyPairTradesHistoryService) Limit(limit int) *CurrencyPairTradesHistoryService {
+	s.limit = &limit
+	return s
+}
+
+func (s *CurrencyPairTradesHistoryService) Offset(offset int) *CurrencyPairTradesHistoryService {
+	s.offset = &offset
+	return s
+}
